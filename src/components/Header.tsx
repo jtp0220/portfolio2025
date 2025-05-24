edea5b272bdad021bf23aac467fd1852b9c39829
Jeremy Tran Perez <jeremytranperez@gmail.com>
2025-05-24 14:26:39 -0400

    added ts for mobile navbar/sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
 
 function Logo(){
@@ -17,7 +17,7 @@ function Tab({title, to} : {title:string, to:string}){
     );
 }
 
-function Navbar(){
+function Navbar({isVisible, setIsVisible} : {isVisible:boolean, setIsVisible: (value: boolean) => void}){
     const tabs = [
         {title: "Home", to: "/"},
         {title: "About", to: "/about"},
@@ -27,11 +27,11 @@ function Navbar(){
     ]
 
     return (
-        <nav>
+        <nav className={isVisible ? 'open' : ''}>
             <ul className="tab-list">
                 {
                     tabs.map((tab, index) => (
-                        <li className="tab" key={index}><Tab title={tab.title} to={tab.to}></Tab></li>
+                        <li className="tab" onClick={isVisible ? () => setIsVisible(false) : undefined} key={index}><Tab title={tab.title} to={tab.to}></Tab></li>
                     ))
                 }
             </ul>
@@ -40,22 +40,53 @@ function Navbar(){
 }
 
 function Header(){
+    const [isVisible, setIsVisible] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+
+    const width = useWindowWidth();
+
+    useEffect(() => {
+        if(width > 1000){
+            setIsMobile(false);
+        } 
+
+        if (width <= 1000){
+            setIsMobile(true);
+            setIsVisible(false);
+        }
+    }, [width])
+
+    const toggleNavbar = () => {
+        setIsVisible(!isVisible);        
+    }
+
     return (
         <header id="header">
             <Logo />
-            <Toggle />
-            <Navbar />
+            <div>
+                {
+                    isMobile && (
+                        !isVisible ? (
+                            <div className="toggle-wrapper sandwich"><svg className="toggle" onClick={toggleNavbar} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></div>
+                        ) : (
+                            <div className="toggle-wrapper close"><svg className="toggle" onClick={toggleNavbar} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></div>                    
+                        )
+                    )
+                }
+            </div>
+            <Navbar isVisible={isVisible} setIsVisible={setIsVisible}/>
         </header>
     )
 }
 
-function Toggle(){
-    return (
-        <div>
-            <div className="toggle-wrapper sandwich"><svg className="toggle" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></div>
-            <div className="toggle-wrapper close"><svg className="toggle" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></div>
-        </div>
-    )
+function useWindowWidth(){
+    const [width, setWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
+    return width;
 }
 
 export default Header;
