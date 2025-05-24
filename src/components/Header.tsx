a9830584816b19e6cda16f129d5eed0186f010c0
Jeremy Tran Perez <jeremytranperez@gmail.com>
2025-05-24 11:42:39 -0400

    added styling for mobile navbar/sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,8 +43,18 @@ function Header(){
     return (
         <header id="header">
             <Logo />
+            <Toggle />
             <Navbar />
         </header>
+    )
+}
+
+function Toggle(){
+    return (
+        <div>
+            <div className="toggle-wrapper sandwich"><svg className="toggle" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></div>
+            <div className="toggle-wrapper close"><svg className="toggle" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></div>
+        </div>
     )
 }
 
